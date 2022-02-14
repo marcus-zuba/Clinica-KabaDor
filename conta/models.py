aa57870ab58a71d334247f33a353fc108096d811
Marcus <marcusduraes2010@gmail.com>
2022-02-14 02:56:35 -0300

    improving css and admin home page

--- conta/models.py
+++ conta/models.py
@@ -26,20 +26,26 @@ class Pessoa(models.Model):
   cidade = models.CharField(max_length=50)
   estado = models.CharField(max_length=50)
   def __str__(self):
-    return 'Conta do usuário {}'.format(self.usuario.username)
+    return 'Pessoa {} de email {}'.format(self.nome, self.email)
 
 class Paciente(models.Model):
   pessoa = models.OneToOneField(Pessoa, on_delete=models.CASCADE, related_name='paciente')
   peso = models.FloatField()
   altura = models.FloatField()
   tipo_sanguineo = models.CharField(max_length=3)
+  def __str__(self):
+    return 'Paciente {} de email {}'.format(self.pessoa.nome, self.pessoa.email)
 
 class Funcionario(models.Model):
   pessoa = models.OneToOneField(Pessoa, on_delete=models.CASCADE, related_name='funcionario')
   data_contrato = models.DateField()
   salario = models.FloatField()
+  def __str__(self):
+    return 'Funcionário {} de email {}'.format(self.pessoa.nome, self.pessoa.email)
 
 class Medico(models.Model):
   funcionario = models.OneToOneField(Funcionario, on_delete=models.CASCADE, related_name='medico')
   especialidade = models.CharField(max_length=50)
   crm = models.IntegerField()
+  def __str__(self):
+    return 'Médico {} de email {}'.format(self.funcionario.pessoa.nome, self.funcionario.pessoa.email)
