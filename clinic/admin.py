9c645b31d5280afa22406ac7d2b871237ea28e3d
Marcus <marcusduraes2010@gmail.com>
2022-02-15 02:47:35 -0300

    fix errors

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Agenda
+from .models import Agenda
 
 # Register your models here.
 
--- clinic/forms.py
+++ clinic/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from conta.models import Medico
-from models import Agenda
+from .models import Agenda
 
 class NovoAgendamento(forms.ModelForm):
-  especialidade = forms.ModelChoiceField(queryset=Medico.objects.all().values('especialidade'), initial=0)
   class Meta:
     model = Agenda
-    fields = ('especialidade', 'data', 'horario', 'nome', 'email', 'telefone')
+    fields = ('data', 'horario', 'nome', 'email', 'telefone')
